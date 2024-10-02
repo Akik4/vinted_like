e68a395adebb3c79da57a26e54cd624cd83e7a2b
akik4 <dupontstef80@gmail.com>
2024-10-02 09:11:56 +0200

    feat(docker): container name

--- data/symfony/src/Controller/TestController.php
+++ data/symfony/src/Controller/TestController.php
@@ -5,14 +5,35 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
+
+use App\Entity\Article;
+use App\Form\ArticleFormType;
+
 
 class TestController extends AbstractController
 {
     #[Route('/test', name: 'app_test')]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
-        return $this->render('test/index.html.twig', [
-            'controller_name' => 'TestController',
+        $article = new Article();
+
+        $form = $this->createForm(ArticleFormType::class, $article);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+        
+            $task = $form->getData();
+
+            $entityManager->persist($task);
+            $entityManager->flush();    
+
+            return $this->redirectToRoute('app_home');  
+        }
+
+        return $this->render('test/new.html.twig', [
+            'form' => $form,
         ]);
     }
 }
