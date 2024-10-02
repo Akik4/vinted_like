6af157d96b1c2e052ac515aa790dea9bd4b70375
aurel95 <aurelien.traini@gmail.com>
2024-10-02 15:10:58 +0200

    SearchBar-Filtre

--- data/symfony/src/Controller/HomeController.php
+++ data/symfony/src/Controller/HomeController.php
@@ -5,14 +5,18 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $kiwi = $request->query->get('value');
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'kiwi' => $kiwi
         ]);
     }
+
 }
