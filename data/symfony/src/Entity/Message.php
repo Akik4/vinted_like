48c7f0867b33a65980ac3413986c94f5afb3ab00
akik4 <dupontstef80@gmail.com>
2024-10-03 14:41:48 +0200

    feat(message) : messagerie system

---
+++ data/symfony/src/Entity/Message.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MessageRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: MessageRepository::class)]
+class Message
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $content = null;
+
+    #[ORM\ManyToOne(inversedBy: 'user')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $User = null;
+
+    #[ORM\Column]
+    private ?int $conversation_id = null;
+
+    #[ORM\ManyToOne(inversedBy: 'messageReceived')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $user2 = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): static
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getUserID(): ?User
+    {
+        return $this->User;
+    }
+
+    public function setUserID(?User $User): static
+    {
+        $this->User = $User;
+
+        return $this;
+    }
+
+    public function getConversationId(): ?int
+    {
+        return $this->conversation_id;
+    }
+
+    public function setConversationId(int $conversation_id): static
+    {
+        $this->conversation_id = $conversation_id;
+
+        return $this;
+    }
+
+    public function getUser2(): ?User
+    {
+        return $this->user2;
+    }
+
+    public function setUser2(?User $user2): static
+    {
+        $this->user2 = $user2;
+
+        return $this;
+    }
+}
